a16fe90cb031f5d4ec2b581e7427748122ec2d8a
Bryan Parada <bryan.parada.cea@gmail.com>
2023-01-21 21:47:14 -0300

    FM162 - Floatting cart button

--- lib/src/pages/shoe_page.dart
+++ lib/src/pages/shoe_page.dart
@@ -27,13 +27,15 @@ class shoePage extends StatelessWidget {
               description: "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.",
 
             ),
-
-
+ 
 
                 ],
               )
             )
              ),
+
+
+             AddShoppingCartButton( price: 180.0),
 
 
           
