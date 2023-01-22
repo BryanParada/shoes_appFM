77ddec9723eef4f34797d401cea9ff03e124c2d0
Bryan Parada <bryan.parada.cea@gmail.com>
2023-01-21 21:34:48 -0300

    FM161 - Show Description

--- lib/src/pages/shoe_page.dart
+++ lib/src/pages/shoe_page.dart
@@ -13,7 +13,30 @@ class shoePage extends StatelessWidget {
         children: <Widget>[
           CustomAppBar('For you'),
           SizedBox(height: 20,),
-          ShoeSizePreview(),
+
+          Expanded(
+            child: SingleChildScrollView(
+              physics: BouncingScrollPhysics(),
+              child: Column(
+                children: const <Widget>[
+
+            ShoeSizePreview(),
+
+           ShoeDescription( 
+              title: 'Nike Air Max 720',
+              description: "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.",
+
+            ),
+
+
+
+                ],
+              )
+            )
+             ),
+
+
+          
         ],
       ),
    );
---
+++ lib/src/widgets/shoe_description.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ShoeDescription extends StatelessWidget {
+
+  final String title;
+  final String description;
+
+  const ShoeDescription({
+    super.key, 
+    required this.title, 
+    required this.description
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SizedBox( height: 20,),
+          Text( this.title, style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700),),
+          SizedBox( height: 20,),
+          Text( this.description, style: TextStyle( color: Colors.black54, height: 1.6,), textAlign: TextAlign.justify,),
+        ],
+      ),
+    );
+  }
+}
